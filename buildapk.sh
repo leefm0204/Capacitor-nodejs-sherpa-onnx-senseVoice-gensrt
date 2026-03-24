ba0aaae7cb8153cd5de24d2e2a1bbd47b24450f1
leefm0204 <103090795+leefm0204@users.noreply.github.com>
2026-03-24 20:52:16 +0800

    Update buildapk.sh

--- buildapk.sh
+++ buildapk.sh
@@ -1,4 +1,5 @@
 rm -rf dist
+rm -rf capacitor-nodejs/android/.cxx capacitor-nodejs/android/build
 export SHERPA_ONNX_C_API_ONNXRUNTIME_DIR=~/Capacitor-nodejs-sherpa-onnx-senseVoice-gensrt/node-addon-api/sherpa-onnx-c-api-onnxruntime
 
 export PATH=$PATH:$PWD/node_modules/.bin
